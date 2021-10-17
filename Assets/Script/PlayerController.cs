a4d9e9fee0dca6dafbf38d6d9a1f9c667a58a403
nitta <yosshi3800@gmail.com>
2021-10-17 14:35:28 +0900

    プレイヤーの向きに合わせて進めるようにしました。

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputStatus m_inputStatus;
+    [SerializeField] float m_acceleration;
+    Transform m_transform;
+    Rigidbody m_rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_transform = this.GetComponent<Transform>();
+        m_rigidbody = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //入力形式
+        switch (m_inputStatus)
+        {
+            case InputStatus.PC:
+                if (Input.GetKey("w"))
+                {
+                    m_rigidbody.velocity += m_transform.TransformDirection(Vector3.forward * m_acceleration) * Time.fixedDeltaTime;
+                }
+                if (Input.GetKey("a"))
+                {
+                    m_rigidbody.velocity += m_transform.TransformDirection(Vector3.left * m_acceleration) * Time.fixedDeltaTime;
+                }
+                if (Input.GetKey("s"))
+                {
+                    m_rigidbody.velocity += m_transform.TransformDirection(Vector3.back * m_acceleration) * Time.fixedDeltaTime;
+                }
+                if (Input.GetKey("d"))
+                {
+                    m_rigidbody.velocity += m_transform.TransformDirection(Vector3.right * m_acceleration) * Time.fixedDeltaTime;
+                }
+                break;
+            case InputStatus.SmartPhone:
+
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 入力状態
+    /// </summary>
+    enum InputStatus
+    {
+        PC,
+        SmartPhone
     }
 }
