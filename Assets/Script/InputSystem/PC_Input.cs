c8697f4cca9f68c5fea2d25f31e8226b2c4504bc
nitta <yosshi3800@gmail.com>
2021-10-21 23:42:33 +0900

    Inputsystemフォルダーを作って、コントローラーのベースを作って継承したPC、スマホのInputクラスを作りました。

---
+++ Assets/Script/InputSystem/PC_Input.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>PC入力クラス</summary>
+public class PC_Input : BasePlayerController
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Mouse0))
+        {
+            m_gasPedal = true;
+        }
+        else
+        {
+            m_gasPedal = false;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_leftRotaion = true;
+        }
+        else
+        {
+            m_leftRotaion = false;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Mouse1))
+        {
+            m_BackPedal = true;
+        }
+        else
+        {
+            m_BackPedal = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_rightRotaion = true;
+        }
+        else
+        {
+            m_rightRotaion = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Jump = true;
+        }
+        else
+        {
+            m_Jump = false;
+        }
+    }
+}
---
+++ Assets/Script/InputSystem/SmartPhone_Input.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartPhone_Input : BasePlayerController
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
