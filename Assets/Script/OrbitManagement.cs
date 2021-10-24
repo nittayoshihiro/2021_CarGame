464b9f5e8e1034b3405afbbfa3966ea224288a90
mypace-hyou <hyou290101@gmail.com>
2021-10-24 16:47:47 +0900

    チェックポイントの作成と汎用できるようにした

---
+++ Assets/Script/OrbitManagement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitManagement : MonoBehaviour
+{
+    /// <summary> 現在のチェックポイント</summary>
+    [Header("現在通過したチェックポイント番号")]
+    public float num;
+    /// <summary> 置いてあるチェックポイントの最大数</summary>
+    [Header("最大チェックポイントの数")]
+    public int MaxCheckPoint;
+    ///<summary> チェックポイント全て通ったかどうか</summary>
+    [HideInInspector]
+    public bool allCheckPoint = false;
+    /// <summary> 周回数</summary>
+    public int WeekNum;
+    /// <summary> チェックポイントobjを保管しておく</summary>
+    [HideInInspector]
+    public GameObject[] checkPointObj = new GameObject[50];
+}
