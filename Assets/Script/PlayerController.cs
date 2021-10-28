7ea63eadb09161b98cb64b39d8d232930db5713b
nitta <yosshi3800@gmail.com>
2021-10-28 09:52:24 +0900

    ジャンプ機能を停止して、フラグを立てるだけにしました。

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 /// <summary>
 /// プレイヤーコントローラー
@@ -7,8 +8,9 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float m_power = 20;
     [SerializeField] float m_rotaionpower = 0.5f;
-    [SerializeField] float m_jumppower = 1;
+    [SerializeField] float m_jumptime = 1;
     [SerializeField] GameObject m_SmartPhoneCanvas = null;//後に読み取りで動作するようにする
+    Coroutine m_coroutine;
     Transform m_transform;
     Rigidbody m_rigidbody;
     BasePlayerController m_BasePlayerController;
@@ -64,7 +66,11 @@ public class PlayerController : MonoBehaviour
     /// <summary>右回転</summary>
     void RightRotaion()
     {
-        if (m_rigidbody.velocity != Vector3.zero)
+        if (!m_ground && m_rigidbody.velocity != Vector3.zero)
+        {
+            //ドリフト処理
+        }
+        else if (m_rigidbody.velocity != Vector3.zero)
         {
             m_transform.Rotate(Vector3.down * m_rotaionpower);
         }
@@ -73,7 +79,11 @@ public class PlayerController : MonoBehaviour
     /// <summary>左回転</summary>
     void LeftRotaion()
     {
-        if (m_rigidbody.velocity != Vector3.zero)
+        if (!m_ground && m_rigidbody.velocity != Vector3.zero)
+        {
+            //ドリフト処理
+        }
+        else if (m_rigidbody.velocity != Vector3.zero)
         {
             m_transform.Rotate(Vector3.up * m_rotaionpower);
         }
@@ -82,7 +92,6 @@ public class PlayerController : MonoBehaviour
     /// <summary>バック（後進）</summary>
     void BackPedal()
     {
-        //前進のパワーの1.5倍
         m_rigidbody.velocity += m_transform.TransformDirection(Vector3.back * m_power) * Time.fixedDeltaTime / m_rigidbody.mass;
     }
 
@@ -91,25 +100,19 @@ public class PlayerController : MonoBehaviour
     {
         if (m_ground)
         {
-            m_rigidbody.velocity = Vector3.up * m_jumppower;
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        //地面触れた時
-        if (collision.gameObject.tag == ("Map"))
-        {
-            m_ground = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        //地面を離れた時
-        if (collision.gameObject.tag == ("Map"))
-        {
             m_ground = false;
+            m_coroutine = StartCoroutine(JumpNow(m_jumptime));
         }
+    }
+
+    /// <summary>
+    /// 飛んでいる間
+    /// </summary>
+    /// <param name="waittime">飛んでいる時間</param>
+    /// <returns></returns>
+    IEnumerator JumpNow(float waittime)
+    {
+        yield return new WaitForSeconds(waittime);
+        m_ground = true;
     }
 }
