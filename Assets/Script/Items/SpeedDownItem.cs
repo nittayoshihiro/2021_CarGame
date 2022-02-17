8a9a0e7e5921637e30432fcf8b23083f99a2c618
nitta <yosshi3800@gmail.com>
2022-02-17 13:12:04 +0900

    爆発エフェクトを作成しました

---
+++ Assets/Script/Items/SpeedDownItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedDownItem : MonoBehaviour
+{
+    [SerializeField] float m_downspeed = 0.8f;
+    [SerializeField] float m_effecttime = 1f;
+    [SerializeField] AudioClip m_itemSound = null;
+    AudioSource m_audioSource = null;
+
+    private void Start()
+    {
+        m_audioSource = this.transform.parent.GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Body")
+        {
+            PlayerController playerController = other.transform.parent.gameObject.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.m_speed *=m_downspeed;
+                m_audioSource.PlayOneShot(m_itemSound);
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Script/Items/WallController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WallController : MonoBehaviour
+{
+    [SerializeField] float m_downspeed = 0.8f;
+    [SerializeField] float m_effecttime = 1f;
+    [SerializeField] AudioClip m_itemSound = null;
+    AudioSource m_audioSource = null;
+    GameManager m_gameManager = null;
+
+    private void Start()
+    {
+        m_audioSource = this.transform.parent.GetComponent<AudioSource>();
+        m_gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Body")
+        {
+            PlayerController playerController = other.transform.parent.gameObject.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.PlayerReset();
+                m_audioSource.PlayOneShot(m_itemSound);
+                m_gameManager.GameOver();
+            }
+        }
+    }
+}
